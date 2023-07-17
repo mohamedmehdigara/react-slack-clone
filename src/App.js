c3fc61b65627796f7845929af8d08e1d3cc0c77c
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-17 11:24:56 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Sidebar from "./components/Sidebar";
 import ChannelList from "./components/ChannelList";
 import ChannelMessages from "./components/ChannelMessages";
 import DirectMessages from "./components/DirectMessages";
 import FileSharing from "./components/FileSharing";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import SignupButtons from "./components/SignupButtons";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <Router>
       <div className="app">
-        <Header/>
+        <Header />
         <Sidebar />
         <Routes>
-          <Route path="/channel/:channelId" element={<ChannelList />}/>
-            <Route path="/channelmessages" element={<ChannelMessages />}/>
-            
-          
-          <Route path="/directmessage/:userId" element={<DirectMessages />}/>
-            
-              
-            
-          
-          <Route path="/filesharing" element={<FileSharing />}/>
-            
-              
-            
-          
+          <Route path="/main" element={<Main />} />
+          <Route path="/channel/:channelId" element={<ChannelList />} />
+          <Route path="/channelmessages" element={<ChannelMessages />} />
+          <Route path="/directmessage/:userId" element={<DirectMessages />} />
+          <Route path="/filesharing" element={<FileSharing />} />
         </Routes>
-        <SignupButtons/>
-        <Footer/>
+        <SignupButtons />
+        <Footer />
       </div>
     </Router>
   );
